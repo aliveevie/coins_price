413d349ef0ba3c9bcfdb38a6448a52cc60c21421
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2024-10-30 14:52:59 +0100

    Delating everything

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,36 @@
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
-import './globals.css';
-// import { Toaster } from '@/components/ui/sonner';
+import '@/styles/globals.css';
+import { Toaster } from '@/components/ui/sonner';
 import { cn } from '@/lib/utils';
-import { Web3Provider } from '@/app/providers/webProvider';
+import { Web3Provider } from '@/providers/web3Provider';
 
 export const metadata: Metadata = {
- title: 'React to Web3 Bootcamp',
- description: 'React to Web3 Bootcamp',
+  title: 'Dabl DevCamp',
+  description: 'Become a dApp developer in 2 weeks',
 };
 
 const fontSans = FontSans({
- subsets: ['latin'],
- variable: '--font-sans',
+  subsets: ['latin'],
+  variable: '--font-sans',
 });
 
 export default function RootLayout({
- children,
+  children,
 }: Readonly<{
- children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
- return (
-   <html lang="en">
-     <link rel="icon" href="/favicon.ico" sizes="any" />
-     <body
-       className={cn(
-         'min-h-screen bg-background font-sans antialiased',
-         fontSans.variable
-       )}
-     >
-       <Web3Provider>{children}</Web3Provider>
-     </body>
-   </html>
- );
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <Web3Provider>{children}</Web3Provider>
+        <Toaster richColors />
+      </body>
+    </html>
+  );
 }
